6563f907d70e05ffe1274415d97041b466b30043
Matthieu <mattannequin@gmail.com>
2023-03-30 15:47:09 +0200

    minor change

--- play_agent.py
+++ play_agent.py
@@ -5,7 +5,7 @@ from pettingzoo.classic import connect_four_v3
 
 env = connect_four_v3.env(render_mode='rgb_array')
 env.reset()
-agent = load_agent('qlearner_values.json')
+agent = load_agent('qlearner_values.json', env)
 print(len(agent.q_values))
 agents = [agent,YourAgent()]
 run_episode(env, agents, True, display=True)
--- train.py
+++ train.py
@@ -10,5 +10,6 @@ env = connect_four_v3.env(render_mode='rgb_array')
 env.reset()
 possible_action = {0,1,2,3,4,5,6}
 agents = [QLearner(possible_action, env.observation_space, gamma=gamma, lr=lr),RandomAgent(possible_action, env.observation_space)]
-run_N_episodes(env, agents, N_episodes=100)
+run_N_episodes(env, agents, N_episodes=1000)
 save_agent(agents[0], 'qlearner_values.json')
+print(len(agents[0].q_values))
