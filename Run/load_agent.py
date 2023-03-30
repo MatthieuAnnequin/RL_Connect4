82d2c63c8c7466472c6905d941c7b9b3b5789897
arthurgdn <arthurguedon0@gmail.com>
2023-03-30 15:32:32 +0200

    improve load agent

--- Run/load_agent.py
+++ Run/load_agent.py
@@ -1,15 +1,24 @@
 import json
 import numpy
 from Agents.QLearnerAgent import QLearner
+from pettingzoo.classic import connect_four_v3
 
-def load_agent(path, agent):
-    if agent.name == 'Q-learning': 
-        print("Started Reading JSON file")
-        with open(path, "r") as read_file:
-            print("Converting JSON encoded data into Numpy array")
-            decodedArray = json.load(read_file)
+def load_agent(path):
+    env = connect_four_v3.env(render_mode='rgb_array')
+    env.reset()
+    possible_action = {0,1,2,3,4,5,6}
+    print("Started Reading JSON file")
+    with open(path, "r") as read_file:
+        print("Converting JSON encoded data into Numpy array")
+        decodedArray = json.load(read_file)
 
-        saved_q_values = numpy.asarray(decodedArray["q_values"])
-        for key, value in saved_q_values.flatten()[0].items():
-            agent.q_values[key] = value
-        return agent
+        agent_type = numpy.asarray(decodedArray["agent_type"])
+        gamma = numpy.asarray(decodedArray["gamma"])
+        lr = numpy.asarray(decodedArray["lr"])
+
+        if agent_type == 'Q-learning':
+            agent = QLearner(possible_action, env.observation_space, gamma=gamma, lr=lr)
+            saved_q_values = numpy.asarray(decodedArray["q_values"])
+            for key, value in saved_q_values.flatten()[0].items():
+                agent.q_values[key] = value
+            return agent
--- Run/save_agent.py
+++ Run/save_agent.py
@@ -12,7 +12,7 @@ def save_agent(agent, path):
 
     if agent.name == 'Q-learning':
 
-        numpyData = {"q_values": agent.q_values}
+        numpyData = {"q_values": agent.q_values, "agent_type": agent.name, "gamma": agent.gamma, "lr": agent.lr}
         print("serialize NumPy array into JSON and write into a file")
         with open(path, "w") as write_file:
             json.dump(numpyData, write_file, cls=NumpyArrayEncoder)
